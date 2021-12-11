f93e07758c226aa20d4d125cf8323f0e9803128f
kimmi <kimmi@172.18.134.138>
2021-12-11 17:48:46 +0900

    first commit

---
+++ src/main/java/com/mycom/myapp/bean/BoardVO.java
@@ -0,0 +1,64 @@
+package com.mycom.myapp.bean;
+
+import java.util.Date;
+
+public class BoardVO {
+	private int id;
+	private String category;
+	private String writer;
+	private String gender;
+	private String birthDate;
+	private String content;
+	private String phone;
+	private Date regdate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	
+	
+}
